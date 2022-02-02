f71ae3989808ed6891a111872bb787bfc2fb6e95
Rena <rena.kuai@gmail.com>
2022-02-02 17:29:04 -0600

    JS logic for rock, paper, scissors

---
+++ script.js
@@ -0,0 +1,50 @@
+
+function computerPlay() {
+    let gameOptions = ['rock', 'paper', 'scissors'];
+    let result = gameOptions[Math.floor(Math.random() * gameOptions.length)];
+    return result;
+    };
+
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
+    if ((playerSelection == 'scissors' && computerSelection == 'paper') || (playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock')) {
+        return ('You have won!' + ' ' + playerSelection.toUpperCase() + ' ' + 'beats' + ' ' + computerSelection.toUpperCase() +'.')
+        + (' ' + 'Your score:' + ' ' + ++yourScore + '.' + ' ' + 'Computer score:' + ' ' + computerScore + '.');
+    }
+    else if ((playerSelection == 'paper' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'rock') || (playerSelection == 'rock' && computerSelection == 'paper')) {
+        return ('You have lost!' + ' ' + playerSelection.toUpperCase() + ' ' + 'loses to' + ' ' + computerSelection.toUpperCase() +'.')
+        + (' ' + 'Your score:' + ' ' + yourScore + '.' + ' ' + 'Computer score:' + ' ' + ++computerScore + '.');
+    }
+    else if ((playerSelection == 'paper' && computerSelection == 'paper') || (playerSelection == 'scissors' && computerSelection == 'scissors') || (playerSelection == 'rock' && computerSelection == 'rock')) {
+        return ('You have tied!')
+        + (' ' + 'Your score:' + ' ' + yourScore + '.' + ' ' + 'Computer score:' + ' ' + computerScore + '.');
+    }
+};
+
+function results() {
+    (yourScore > computerScore) ? 'Yay, you won!' :
+    'Boo you lost.';
+}
+
+let yourScore = 0;
+let computerScore = 0;
+let playerPrompt = prompt('Rock, paper, or scissors?', ' ');
+let playerSelection = playerPrompt.toLowerCase();
+console.log(playRound(playerSelection));
+
+playerPrompt = prompt('Rock, paper, or scissors?', ' ');
+console.log(playRound(playerSelection));
+
+playerPrompt = prompt('Rock, paper, or scissors?', ' ');
+console.log(playRound(playerSelection));
+
+playerPrompt = prompt('Rock, paper, or scissors?', ' ');
+console.log(playRound(playerSelection));
+
+playerPrompt = prompt('Rock, paper, or scissors?', ' ');
+console.log(playRound(playerSelection));
+
+console.log(results());
+
+
+
